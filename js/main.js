74eec8d7c6977bcdb708b8c2261414f73a98a0db
kdb <dabin6743@gmail.com>
2023-03-13 20:39:42 +0900

    Feat : 버튼 클릭시 남은시간, 획득점수 초기화

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@
 //사용변수
 let score = 0;
 let time = 3; //타이핑 할 시간 9초
-let isPlaying = true; //useSate 역할
+let isPlaying = false; //useSate 역할
 let interval;
+let checkInterval;
+let words = []
 
 const wordInput = document.querySelector('.word-input'); //사용자 input 값
 const wordDisplay = document.querySelector('.word-display'); //랜덤 단어
@@ -14,32 +16,66 @@ const scoreDisplay = document.querySelector('.score'); //획득점수
 const timeDisplay = document.querySelector('.time')//남은시간
 const button = document.querySelector('.button');
 
-//사용자가 입력한 input값-랜덤값 같으면 획득점수 +1
-wordInput.addEventListener('input',()=>{
+//화면이 렌더링 되었을때 초기값 세팅
+//1. word-display에 배열의 랜덤 요소 들어가기
+
+init()
+
+function init(){
+    getWords();
+    wordInput.addEventListener('input',wordMatch)//단어 일치
+}
+
+
+//단어 불러 오기
+function getWords(){
+    words = ['Hello','Banana','Apple','Cherry']
+    buttonChange('게임시작')
+    gameOver
+}
+
+//게임이 실행중인지 아닌지 실시간으로 확인
+function gameOver(){
+    if(time==0 && !isPlaying){
+        buttonChange('게임종료')
+    }
+}
+
+
+//단어 일치 체크
+function wordMatch(){
     if(wordInput.value.toLowerCase()===wordDisplay.innerText.toLowerCase()){
+        wordInput.value=''//조건 맞을때 input창 초기화 시키기
+        if(!isPlaying){ //return때문에 아래 코드들 실행 안돼서 획득점수 안올라감
+            return
+        }
         score++
         scoreDisplay.innerText=score; //증가된값을 획득점수에 update
-        wordInput.value=''//조건 맞을때 input창 초기화 시키기
+        const randomIndex=Math.floor(Math.random() * words.length) //단어의 길이만큼 곱하면 그안에서 랜덤한 숫자 나옴
+        wordDisplay.innerText=words[randomIndex]
     }
-})
-
-buttonChange('게임시작')
-
-//button onclick 함수
-//버튼 누르면 카운트함수 1초에 한번씩 실행
-function run(){
-    time=3; //버튼 누를때마다 카운트 다시 되어야함
-    interval = setInterval(countDown,1000);    
 }
+
+
+//버튼 누르면 실행하는 함수, onclick에 넣음
+function run(){
+    time=3; //카운트 3부터 다시
+    scoreDisplay.innerText=0;//점수 0부터 다시
+    wordInput.focus()//input에 자동으로 커서 생김
+    interval = setInterval(countDown,1000);
+    checkInterval = setInterval(gameOver,50)
+    buttonChange('게임중')
+}
+
 
 
 //카운트다운 함수 -> time이 0 이상이면 1씩 감소
 function countDown(){
-    time > 0 ? time -- : clearInterval(interval);
-    //isPlaying=flase이면 interval 함수 stop
-    // if(!isPlaying){
-    //     clearInterval(interval)
-    // }
+    isPlaying=true
+    time > 0 ? time -- : isPlaying=false;
+    if(!isPlaying){
+        clearInterval(interval)
+    }
     console.log(time)
     timeDisplay.innerText=time; //남은시간에 넣어줌
 }
